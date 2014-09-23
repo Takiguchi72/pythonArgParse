365b4de67ab60e8db5cdc2505328ce42f27f4754
Takiguchi <takiguchi@Taki-Debian>
2014-09-23 16:10:01 +0200

    Implémentation des paramètres positionnels

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("echo")
+# Gestion des arguments
+# La durée doit être renseignée avec les arguments "temps" et "valeur_temps" 
+parser.add_argument("temps")                                        # Indique qu'on renseigne la duree
+parser.add_argument("tempsValeur", help = "Valeur en minutes")     # Renseigne la duree désiree
+
 args = parser.parse_args()
