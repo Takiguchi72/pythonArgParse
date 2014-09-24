c41c16334f1925bfbca529161b66eb1f59b003d1
Takiguchi <takiguchi@Taki-Debian>
2014-09-24 15:04:35 +0200

    Ajout d'une fonction qui vérifie que le deuxième sous argument est un entier

--- main.py
+++ main.py
@@ -9,45 +9,37 @@ parser.add_argument("temps_playlist", type=int,  help = "Spécifie la durée de 
 
 # Gestion des arguments optionnels
 ''' Paramètre permettant de renseigner le format de playliste désiré selon la liste {m3u,xspf,pls} '''
-parser.add_argument("--format", choices=['m3u','xspf','pls'], help = "Donne le format de sortie de la playliste (m3u | xspf | pls)")
+parser.add_argument("--format", nargs = 1, choices=['m3u','xspf','pls'], help = "Donne le format de sortie de la playliste (m3u | xspf | pls)")
 # ToDo -> gèrer les paramètres et sous paramètres avec les <Arguments parents> Subparsers -> paramètre "nargs"
+
     # Gestion Titre
 ''' Paramètre permettant de renseigner le pourcentage d'un titre dans la playliste [entier naturel] (Ex: "60" -> 60%) '''
-parser.add_argument("--titre", help = "Indique qu'on spécifie le pourcentage d'un titre dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
-''' Sous paramètre indiquant le titre désiré [chaine] '''
-parser.add_argument("--strTitre", help = "Chaine correspondante au titre désiré")
-''' Sous paramètre indiquant le pourcentage du titre désiré '''
-parser.add_argument("--intTitre", type=int, help = "Entier naturel correspondant au pourcentage du titre désiré")
+parser.add_argument("--titre", nargs = 2, help = "Indique qu'on spécifie le pourcentage d'un titre dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
+
     # Gestion du genre
 ''' Paramètre permettant de renseigner le pourcentage d'un genre dans la playliste [entier naturel] (Ex: "60" -> 60%) '''
-parser.add_argument("--genre", help = "Indique qu'on spécifie le pourcentage d'un genre dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
-''' Sous paramètre indiquant le genre désiré [chaine] '''
-parser.add_argument("--strGenre", help = "Chaine correspondante au genre désiré")
-''' Sous paramètre indiquant le pourcentage du genre désiré '''
-parser.add_argument("--intGenre", type=int, help = "Entier naturel correspondant au pourcentage du genre désiré")
+parser.add_argument("--genre", nargs = 2, help = "Indique qu'on spécifie le pourcentage d'un genre dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
+
+
     # Gestion du sous-genre
 ''' Paramètre permettant de renseigner le pourcentage d'un sous-genre dans la playliste [entier naturel] (Ex: "60" -> 60%) '''
-parser.add_argument("--sousgenre", help = "Indique qu'on spécifie le pourcentage d'un sous-genre dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
-''' Sous paramètre indiquant le sous genre désiré [chaine] '''
-parser.add_argument("--strSousgenre", help = "Chaine correspondante au sous-genre désiré")
-''' Sous paramètre indiquant le pourcentage du sous genre désiré '''
-parser.add_argument("--intSousgenre", type=int, help = "Entier naturel correspondant au pourcentage du sous-genre désiré")
+parser.add_argument("--sousgenre", nargs = 2, help = "Indique qu'on spécifie le pourcentage d'un sous-genre dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
+
+
     # Gestion de l'artiste
 ''' Paramètre permettant de renseigner le pourcentage d'un artiste dans la playliste [entier naturel] (Ex: "60" -> 60%) '''
-parser.add_argument("--artiste", help = "Indique qu'on spécifie le pourcentage d'un artiste dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
-''' Sous paramètre indiquant l'ariste désiré [chaine] '''
-parser.add_argument("--strArtiste", help = "Chaine correspondante à l'artiste désiré")
-''' Sous paramètre indiquant le pourcentage de l'artiste désiré '''
-parser.add_argument("--intArtiste", type=int, help = "Entier naturel correspondant au pourcentage de l'artiste désiré")
+parser.add_argument("--artiste", nargs = 2, help = "Indique qu'on spécifie le pourcentage d'un artiste dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
+
     # Gestion de l'album
 ''' Paramètre permettant de renseigner le pourcentage d'un album dans la playliste [entier naturel] (Ex: "60" -> 60%) '''
-parser.add_argument("--album", help = "Indique qu'on spécifie le pourcentage d'un album dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
-''' Sous paramètre indiquant l'album désiré [chaine] '''
-parser.add_argument("--strAlbum", help = "Chaine correspondante à l'album désiré")
-''' Sous paramètre indiquant le pourcentage de l'album désiré '''
-parser.add_argument("--intAlbum", type=int, help = "Entier naturel correspondant au pourcentage de l'album désiré")
+parser.add_argument("--album", nargs = 2, help = "Indique qu'on spécifie le pourcentage d'un album dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
 
 args = parser.parse_args()
 
-print("youpi")
+
+''' Fonction de vérification des sous arguments '''
+def checkSousArgs(arg):
+    return int(arg[1])
+    
+checkSousArgs(args.genre)
 
