ec8a429961a169d275457a7322f86dc3f074ec5a
Takiguchi <takiguchi@Taki-Debian>
2014-09-24 16:58:47 +0200

    Ajout d'une boucle qui vérifie tous les arguments ayant 2 sous-arguments, et si ils sont renseignés dans la cmd

--- main.py
+++ main.py
@@ -6,47 +6,38 @@ parser.add_argument("nom_playlist", help = "Le nom du fichier contenant la playl
 # ToDo -> voir pour associer une fonction de vérification à la déclaration d'un argument -> paramètre "action" cf doc
 ''' Le 2ème paramètre correspond à la durée désirée pour la playliste [entier naturel] '''
 parser.add_argument("temps_playlist", type=int,  help = "Spécifie la durée de la playlist, en minutes. [entier naturel] (Ex : 60)")
-
 # Gestion des arguments optionnels
+    # Gestion Format
 ''' Paramètre permettant de renseigner le format de playliste désiré selon la liste {m3u,xspf,pls} '''
 parser.add_argument("--format", nargs = 1, choices=['m3u','xspf','pls'], help = "Donne le format de sortie de la playliste (m3u | xspf | pls)")
-# ToDo -> gèrer les paramètres et sous paramètres avec les <Arguments parents> Subparsers -> paramètre "nargs"
-
     # Gestion Titre
 ''' Paramètre permettant de renseigner le pourcentage d'un titre dans la playliste [entier naturel] (Ex: "60" -> 60%) '''
 parser.add_argument("--titre", nargs = 2, help = "Indique qu'on spécifie le pourcentage d'un titre dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
-
     # Gestion du genre
 ''' Paramètre permettant de renseigner le pourcentage d'un genre dans la playliste [entier naturel] (Ex: "60" -> 60%) '''
 parser.add_argument("--genre", nargs = 2, help = "Indique qu'on spécifie le pourcentage d'un genre dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
-
-
     # Gestion du sous-genre
 ''' Paramètre permettant de renseigner le pourcentage d'un sous-genre dans la playliste [entier naturel] (Ex: "60" -> 60%) '''
 parser.add_argument("--sousgenre", nargs = 2, help = "Indique qu'on spécifie le pourcentage d'un sous-genre dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
-
-
     # Gestion de l'artiste
 ''' Paramètre permettant de renseigner le pourcentage d'un artiste dans la playliste [entier naturel] (Ex: "60" -> 60%) '''
 parser.add_argument("--artiste", nargs = 2, help = "Indique qu'on spécifie le pourcentage d'un artiste dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
-
     # Gestion de l'album
 ''' Paramètre permettant de renseigner le pourcentage d'un album dans la playliste [entier naturel] (Ex: "60" -> 60%) '''
 parser.add_argument("--album", nargs = 2, help = "Indique qu'on spécifie le pourcentage d'un album dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
 
 args = parser.parse_args()
 
-
 ''' Fonction de vérification des sous arguments '''
-def checkSousArgs(arg):
+def checkSousArgs(arg, nomArg):
     try:
         nb = int(arg[1])
         if (checkIntNatural(nb) == False):
-            print ("La quantité doit être positive ! (" + arg[1] + ")")
+            print ("Argument --" + nomArg + " incorrect !\n\t la quantité doit être positive ! (" + arg[1] + ")")
         return 0
     except ValueError:
-        print ("Impossible de convertir \"" + arg[1] + "\" en nombre entier !")
-   
+        print ("Erreur : --" + nomArg + ", impossible de convertir \"" + arg[1] + "\" en nombre entier !")
+
 ''' Vérifie qu'un nombre est un entier naturel '''     
 def checkIntNatural(nb):
     if nb > 0:
@@ -54,6 +45,7 @@ def checkIntNatural(nb):
     else:
         return False
 
-
-checkSousArgs(args.genre)
-
+''' Boucle pour tester l'ensemble des paramètres optionnels, et réalise le test uniquement si l'argument est renseigné '''
+for ARG in ['titre','genre','sousgenre','artiste','album']:
+    if getattr(args, ARG) is not None:
+        checkSousArgs(getattr(args, ARG), ARG)
