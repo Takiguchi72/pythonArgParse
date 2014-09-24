0ab21c8f3c1551f99b8934df2828d2c43ff4b335
Takiguchi <takiguchi@Taki-Debian>
2014-09-24 15:03:17 +0200

    Association des sous arguments avec la fonction ''nargs = x''

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ parser = argparse.ArgumentParser()
 # Gestion des arguments positionnels
 ''' Le 1er paramètre correspond au nom que l'on veut attribuer à la playliste [chaine] (Ex: "maPlayliste") '''
 parser.add_argument("nom_playlist", help = "Le nom du fichier contenant la playlist.")
-# ToDo -> voir pour associer une fonction de vérification à la déclaration d'un argument
+# ToDo -> voir pour associer une fonction de vérification à la déclaration d'un argument -> paramètre "action" cf doc
 ''' Le 2ème paramètre correspond à la durée désirée pour la playliste [entier naturel] '''
 parser.add_argument("temps_playlist", type=int,  help = "Spécifie la durée de la playlist, en minutes. [entier naturel] (Ex : 60)")
 
 # Gestion des arguments optionnels
 ''' Paramètre permettant de renseigner le format de playliste désiré selon la liste {m3u,xspf,pls} '''
 parser.add_argument("--format", choices=['m3u','xspf','pls'], help = "Donne le format de sortie de la playliste (m3u | xspf | pls)")
+# ToDo -> gèrer les paramètres et sous paramètres avec les <Arguments parents> Subparsers -> paramètre "nargs"
     # Gestion Titre
 ''' Paramètre permettant de renseigner le pourcentage d'un titre dans la playliste [entier naturel] (Ex: "60" -> 60%) '''
 parser.add_argument("--titre", help = "Indique qu'on spécifie le pourcentage d'un titre dans la playliste. [entier naturel] (Ex: 30 -> 30%%)")
