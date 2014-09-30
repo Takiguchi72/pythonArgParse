9a3fdeb1bb826de8f3e6852fb63fe3ab93155522
Takiguchi <takiguchi@Taki-Debian>
2014-09-30 17:13:25 +0200

    Creation d'un fichier de log, et écriture d'une erreur dans le fichier de log lorsque le 2eme sous argument d'un argument n'est pas un entier naturel

--- main.py
+++ main.py
@@ -4,16 +4,29 @@ def checkSousArgs(arg, nomArg):
     try:
         # Conversion en entier
         nb = int(arg[1])
+        # Si nb n'est pas un entier naturel et qu'il est supérieur ou égal à 100, on lève une exception !
         if (checkIntNatural(nb) == False):
-            print ("Argument --" + nomArg + " incorrect !\n\t la quantité doit être positive ! (" + arg[1] + ")")
-        # Si il n'y a pas d'erreur, on retourne la valeur 0
-        return 0
-    except ValueError:
-        print ("Erreur : --" + nomArg + ", impossible de convertir \"" + arg[1] + "\" en nombre entier !")
+            raise Exception('EntierNaturel',' --' + nomArg + ', la valeur "' + arg[1] + '" doit être positive !')
 
-''' Vérifie qu'un nombre est un entier naturel '''     
+        # Si il n'y a pas d'erreur, on retourne la valeur 0
+        else:
+            return 0
+    except Exception as er:
+        if er.args[0] == 'EntierNaturel':
+            logging.error(er.args[1])
+        else:
+            logging.error(' --' + nomArg + ', impossible de convertir "' + arg[1] + '" en nombre entier !')
+
+''' Vérifie qu'un nombre est un entier naturel '''
 def checkIntNatural(nb):
     if nb > 0:
+        return True
+    else:
+        return False
+    
+''' Vérifie qu'un nombre est inférieur à 100 '''
+def checkIntInfCent(nb):
+    if nb < 100:
         return True
     else:
         return False
@@ -21,13 +34,16 @@ def checkIntNatural(nb):
 ''' Traitement du programme principal '''
 import argparse
 import logging
-parser = argparse.ArgumentParser()
+
+parser = argparse.ArgumentParser()          # Création d'un objet de classe ArgumentParser
+logging.basicConfig(filename='errors.log', level=logging.DEBUG)   # Les erreurs seront redirigées dans le fichier de log nommé 'errors.log'
+
 # Gestion des arguments positionnels
 ''' Le 1er paramètre correspond au nom que l'on veut attribuer à la playliste [chaine] (Ex: "maPlayliste") '''
 parser.add_argument("nom_playlist", help = "Le nom du fichier contenant la playlist.")
-# ToDo -> voir pour associer une fonction de vérification à la déclaration d'un argument -> paramètre "action" cf doc
 ''' Le 2ème paramètre correspond à la durée désirée pour la playliste [entier naturel] '''
 parser.add_argument("temps_playlist", type=int,  help = "Spécifie la durée de la playlist, en minutes. [entier naturel] (Ex : 60)")
+
 # Gestion des arguments optionnels
     # Gestion Format
 ''' Paramètre permettant de renseigner le format de playliste désiré selon la liste {m3u,xspf,pls} '''
@@ -50,12 +66,15 @@ parser.add_argument("--album", nargs = 2, help = "Indique qu'on spécifie le pou
 
 args = parser.parse_args()
 
-''' Boucle pour tester l'ensemble des paramètres optionnels, et réalise le test uniquement si l'argument est renseigné '''
-for ARG in ['titre','genre','sousgenre','artiste','album']:
-    if getattr(args, ARG) is not None:
-        checkSousArgs(getattr(args, ARG), ARG)
+#''' Boucle pour tester l'ensemble des paramètres optionnels, et réalise le test uniquement si l'argument est renseigné '''
+# for ARG in ['titre','genre','sousgenre','artiste','album']:
+#     if getattr(args, ARG) is not None:
+#         checkSousArgs(getattr(args, ARG), ARG)
 
-
-
-
+# Gestion de l'argument genre et ses sous-arguments
+logging.info('Argument --genre : ' + args.genre[0] + ' ' + args.genre[1])
+# On vérifie que le 2em sous argument de genre est bien un entier naturel
+if checkSousArgs(args.genre,'genre') == 0:
+    print ('ok')
+    
 # la commande exit(0) permet de quitter le programme sans omettre d'erreur, alors que exit(1) lève une erreur
